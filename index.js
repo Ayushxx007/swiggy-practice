1df48d1045f59d2ba1f33d262d241f0c1f75d3c2
“Ayush <ayushkumawat77889@gmail.com>
2025-03-11 07:49:24 +0530

    ddd

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+
+
+let element=React.createElement('h1', {id:"mahika"}, "Hello, world!");
+let root =ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
